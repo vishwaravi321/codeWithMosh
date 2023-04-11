a076582843b65b19a0185fbf75dbf5275a4d6368
vishwa <vishwa.r@nxweb.in>
2023-04-11 12:33:06 +0530

    optomized

--- pattern.py
+++ pattern.py
@@ -1,11 +1,4 @@
 c = '*****'
 
-print(c[0:1])
-print(c[0:2])
-print(c[0:3])
-print(c[0:4])
-print(c[0:5])
-print(c[0:4])
-print(c[0:3])
-print(c[0:2])
-print(c[0:1])
+for i in range(len(c)):
+    print(f"{c[0:i]}")
